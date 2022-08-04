7049192e5f3cf933675b4348188bdd14099b9340
danber <bergauzen.danil@mail.ru>
2022-08-05 00:30:50 +0300

    fix heroku refresh

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.resolve(__dirname, "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.js"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 io.on("connection", (socket) => {
